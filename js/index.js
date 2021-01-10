6319e2be05d06b68c1b9789c8041e2792dc7a042
Kelvin Lin <kelvindlin97@gmail.com>
2021-01-10 18:09:29 -0500

    Updated index.js (added level order traversal

--- js/index.js
+++ js/index.js
@@ -1,21 +1,43 @@
 /* eslint-disable require-jsdoc */
-'use strict';
+"use strict";
 
 // eslint-disable-next-line no-unused-vars
 function parseHTML(userInputString) {
-  if (userInputString !== '') { // TODO will userInputString ever be null?
+  if (userInputString !== "") {
+    // TODO will userInputString ever be null?
     const parser = new DOMParser();
     // TODO error check parseFromString - adapt to different browsers - https://developer.mozilla.org/en-US/docs/Web/API/DOMParser#DOMParser_HTML_extension
-    const htmlDocument = parser.parseFromString(userInputString, 'text/html');
-    let node = htmlDocument.documentElement;
-    // Sample code to traverse first children until leaf
-    let treeLevel = 0;
-    while (node.childElementCount !== 0) {
-      // Replace the line below with console.log(node) to see the object
-      console.log(`Node on level ${treeLevel}: ${node.tagName}`);
-      treeLevel++;
-      node = node.children[0];
-    }
-    console.log(`Node on level ${treeLevel}: ${node.tagName}`);
+    const htmlDocument = parser.parseFromString(userInputString, "text/html");
+    const node = htmlDocument.documentElement;
+
+    const nodeListLevelOrder = levelOrderTraversal(node);
+    const d3Dataset = d3DataFormatter(nodeListLevelOrder);
+    return d3Dataset;
   }
 }
+
+function levelOrderTraversal(rootNode) {
+  const array = [];
+  search(rootNode, 1);
+
+  function search(root, level) {
+    if (root) {
+      if (array.length < level) {
+        array.push([]);
+      }
+      const arr = array[level - 1];
+      arr.push(root.tagName);
+      search(root.children[0], level + 1);
+      search(root.children[1], level + 1);
+    } else {
+      return;
+    }
+  }
+  return array;
+}
+
+// function d3DataFormatter(node) {
+//   const rootObj = { name: node[0], children: [] };
+//   const arr = [rootObj];
+//   console.log(arr);
+// }
