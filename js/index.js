a8473f695c2fddc12a2e43d6b9ba83b82d8e2c56
Kelvin Lin <kelvindlin97@gmail.com>
2021-01-18 23:00:46 -0500

    Working on JSON conversion

--- js/index.js
+++ js/index.js
@@ -10,8 +10,7 @@ function parseHTML(userInputString) {
     const htmlDocument = parser.parseFromString(userInputString, "text/html");
     const node = htmlDocument.documentElement;
     const nodeListLevelOrder = levelOrderTraversal(node);
-    const d3Dataset = d3DataFormatter(nodeListLevelOrder);
-    return d3Dataset;
+    console.log(nodeListLevelOrder);
   }
 }
 
@@ -20,12 +19,19 @@ function levelOrderTraversal(rootNode) {
   search(rootNode, 1);
 
   function search(root, level) {
-    if (root) {
+    if (root !== undefined) {
       if (array.length < level) {
         array.push([]);
       }
       const arr = array[level - 1];
-      arr.push(root.tagName);
+      arr.push({ name: root.tagName });
+
+      if (level === 1) {
+      }
+
+      if (root.children !== undefined) {
+        arr[arr.length - 1].children = [];
+      }
 
       for (let i = 0; i < root.children.length; i++) {
         search(root.children[i], level + 1);
@@ -37,18 +43,37 @@ function levelOrderTraversal(rootNode) {
   return array;
 }
 
-function d3DataFormatter(nodeList) {
-  const formattedData = [];
-  //Every object should have a childrens property unless we're on the last nested array within 2d array
-  //Every object should have a parent's property unless we're on the first nested array within 2d array
-  for (let i = 0; i < nodeList.length; i++) {
-    const levelNodes = nodeList[i];
-    for (let j = 0; j < levelNodes.length; j++) {
-      console.log(levelNodes);
-      let tagName = levelNodes[i];
-      formattedData[i] = { name: tagName };
-    }
-  }
-  console.log(formattedData);
-  return formattedData;
-}
+// function JSONConverter(rootNode) {
+//   //rootNode is HTML tag
+//   const array = [];
+
+//   //rootNode = <script>
+
+//   if (rootNode.children === undefined) {
+//     let arr
+//     while(array.children !== undefined){
+//       let arr = array.children
+//     }
+
+//     array[array.length - 1]children.push({
+//       name: rootNode.tagName,
+//       parent: array[array.length - 1].tagName,
+//     });
+//   }
+
+//   if (rootNode.children !== undefined) {
+//     array.push({ name: rootNode.tagName, children: [] });
+
+//     for (let i = 0; i < rootNode.children.length; i++) {
+//       array[array.length - 1].children.push({
+//         name: rootNode.children[i].tagName,
+//         parent: rootNode.tagName,
+//         children: [],
+//       });
+
+//       JSONConverter(rootNode.children[i]);
+//     }
+//   }
+
+//   return array;
+// }
