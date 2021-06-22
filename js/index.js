d770799138cf14209a97ab05179cc8995bff22d1
Kelvin Lin <69781419+kelvinlin97@users.noreply.github.com>
2021-06-22 12:16:35 -0400

    Color nodes based on HTML type and modify Visualize.js (Zoom, Node spacing, and Tooltip)  (#48)
    * Create UI/UX for DOM Tree (Tooltip, Zoom, Node Position, Color-Coordinating)
    
    Co-authored-by: Edward <14011954+0xedward@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
+
 /* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
-'use strict';
+"use strict";
 
 // TODO consider if using d3.select("svg").size() == 0 instead is a good idea
-let isTreeOnDOM = false;
 
 function generateDOMTree(userInputString) {
   const parserOutputNode = parseHTML(userInputString);
   if (parserOutputNode !== null) {
     const d3TreeData = levelOrderTraversal(parserOutputNode);
-    const DOMTreeRootNode = d3TreeData[0];
-    // TODO performance: cache the previous input string to check
-    // if the current string is the same before traversing the DOM Tree
-    if (isTreeOnDOM === false) {
-      createAndAppendDOMTree(DOMTreeRootNode);
-      isTreeOnDOM = true;
+    const DOMTreeRootNode = d3TreeData;
+
+    if (d3.select('svg').size() === 0) {
+      new DOMTree(DOMTreeRootNode);
     } else {
-      removeNodes();
-      createAndAppendDOMTree(DOMTreeRootNode);
+      d3.select('svg').remove();
+      d3.select('div.tooltip').remove();
+      new DOMTree(DOMTreeRootNode);
     }
   } else {
     // TODO remove this exception when we add try catch block to parseHTML
@@ -38,10 +37,8 @@ function parseHTML(userInputString) {
 
 function levelOrderTraversal(rootNode) {
   // Level order traverse the output of DOMParser
-  const resultArray = [{
-    name: 'HTML',
-    children: [],
-  }];
+  const resultArray = [{name: 'HTML', children: [],
+    type: 'Doctype', color: '#CBEEF3'}];
   levelOrderTraversalHelper(rootNode, resultArray[0].children, resultArray);
   return resultArray;
 }
@@ -60,12 +57,16 @@ function levelOrderTraversalHelper(node, childrenArr, outputArray) {
     const isNotHTMLNode = node.parentNode.nodeName !== '#document';
     const currentNodeElementName = node.tagName;
     const currentNodeParentElementName = node.parentNode.tagName;
+    const {type, color} = determineType(currentNodeElementName);
+
     if (isNotHTMLNode) {
       childrenArr.push({
         name: currentNodeElementName,
         parent: currentNodeParentElementName,
         children: [],
         obj: node,
+        type: type,
+        color: color,
       });
     } else if (isLeaf) {
       childrenArr.push({
@@ -73,6 +74,8 @@ function levelOrderTraversalHelper(node, childrenArr, outputArray) {
         // TODO do we need this reference?
         parent: currentNodeParentElementName,
         obj: node,
+        type: type,
+        color: color,
       });
     }
     if (!isLeaf) {
@@ -108,6 +111,69 @@ function levelOrderTraversalHelper(node, childrenArr, outputArray) {
   }
 }
 
+function determineType(node) {
+  const document = 'HTML';
+  const docMetadata = new Set(['HEAD', 'TITLE', 'BASE', 'LINK', 'META', 'STYLE']);
+  const sections = new Set(['BODY', 'ARTICLE', 'SECTION', 'NAV', 'ASIDE', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'HGROUP', 'HEADER', 'FOOTER', 'ADDRESS']);
+  const grouping = new Set(['P', 'HR', 'PRE', 'BLOCKQUOTE', 'OL', 'UL', 'MENU', 'LI', 'DL', 'DT', 'DD', 'FIGURE', 'FIGCAPTION', 'MAIN', 'DIV']);
+  const textSemantics = new Set(['A', 'EM', 'STRONG', 'SMALL', 'S', 'CITE', 'Q', 'DFN', 'ABBR', 'RUBY', 'RT', 'RP', 'DATA', 'TIME', 'CODE', 'VAR', 'SAMP', 'KBD', 'SUB', 'SUP', 'I', 'B', 'U', 'MARK', 'BDI', 'BDO', 'SPAN', 'BR', 'WBR']);
+  const links = new Set(['AREA', 'ALTERNATE', 'AUTHOR', 'BOOKMARK', 'CANONICAL', 'DNS-PREFETCH', 'EXTERNAL', 'HELP', 'ICON', 'LICENSE', 'MANIFEST', 'MODULEPRELOAD', 'NOFOLLOW', 'NOREFERRER', 'OPENER', 'PINGBACK', 'PRECONNECT', 'PREFETCH', 'PRELOAD', 'PRERENDER', 'SEARCH', 'STYLESHEET', 'TAG', 'NEXT', 'PREV']);
+  const edits = new Set(['INS', 'DEL']);
+  const embeddedContent = new Set(['PICTURE', 'SOURCE', 'IMG', 'SOURCE', 'LINK', 'IFRAME', 'EMBED', 'OBJECT', 'PARAM', 'VIDEO', 'AUDIO', 'TRACK', 'MAP', 'AREA']);
+  const tabularData = new Set(['TABLE', 'CAPTION', 'COLGROUP', 'COL', 'TBODY', 'THEAD', 'TFOOT', 'TR', 'TD', 'TH']);
+  const forms = new Set(['FORM', 'LABEL', 'INPUT', 'BUTTON', 'SELECT', 'DATALIST']);
+
+  let type;
+  let color;
+
+  switch (true) {
+    case document === node:
+      type = 'Document Type';
+      color = '#FFFFFF';
+      break;
+    case docMetadata.has(node):
+      type = 'Document Metadata';
+      color = '#7CA982';
+      break;
+    case sections.has(node):
+      type = 'Sections';
+      color = '#E2C2FF';
+      break;
+    case grouping.has(node):
+      type = 'Grouping Content';
+      color = '#A7ADC6';
+      break;
+    case textSemantics.has(node):
+      type = 'Text-Level Semantics';
+      color = '#5BC0EB';
+      break;
+    case links.has(node):
+      type = 'Links';
+      color = '#F4F4F9';
+      break;
+    case edits.has(node):
+      type = 'Edits';
+      color = '#F3D9B1';
+      break;
+    case embeddedContent.has(node):
+      type = 'Embedded Content';
+      color = '#311E10';
+      break;
+    case tabularData.has(node):
+      type = 'Tabular Data';
+      color = '#759AAB';
+      break;
+    case forms.has(node):
+      type = 'Forms';
+      color = '#FFE787';
+      break;
+    default:
+      type = 'Other';
+      color = '#3066BE';
+  }
+  return {'type': type, 'color': color};
+}
+
 // eslint-disable-next-line no-unused-vars
 function liveUpdate() {
   const userInputString = document.getElementById('html-input-box').value;
@@ -118,3 +184,4 @@ function liveUpdate() {
     generateDOMTree(userInputString);
   }
 }
+
