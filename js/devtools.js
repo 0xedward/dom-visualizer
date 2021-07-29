8937dbe138024bbae0a36727c7d70cf19af0e2da
Kelvin Lin <69781419+kelvinlin97@users.noreply.github.com>
2021-07-29 10:30:19 -0400

    Setup manifest.json and extension

---
+++ js/devtools.js
@@ -0,0 +1,5 @@
+// chrome.devtools.panel.elements.createSideBarPane("DOM Tree", function(sidebar) {
+//   sidebar.setPage(`dom-treemap.html`)
+// })
+
+// chrome.devtools.panel.create(`DOM Tree Visualizer Stats`, `icon128.png`, `component`)
